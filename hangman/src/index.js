e93460c85eaedff96d49977ef4d2dc3090f45cad
Streakfull <34299200+Streakfull@users.noreply.github.com>
2018-07-25 15:13:21 +0200

    habd2

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 ///
-class Main extends React.Component{
-  constructor(props){
-    super(props)
-  }
-}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-    super(props)
-  }
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
