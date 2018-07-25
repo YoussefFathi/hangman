4eded69169b83e7b59de49a7cdefb3080e0894f2
Streakfull <34299200+Streakfull@users.noreply.github.com>
2018-07-25 15:00:28 +0200

    secondTry

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+///
+class Main extends React.Component{
+  constructor(props){
+    super(props)
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
